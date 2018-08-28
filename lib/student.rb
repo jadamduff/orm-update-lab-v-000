f8461ba9e61bf46855d3b734b8bc22bb5f942029
Adam Duff <j.adam.duff@gmail.com>
2018-08-28 23:57:09 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,9 +1,23 @@
 require_relative "../config/environment.rb"
 
 class Student
+  attr_accessor :name, :grade
+  attr_reader :id
 
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]
+  def initialize(name, grade)
+    @name = name
+    @grade = grade
+    @id = nil
+  end
 
+  def self.create_table
+    sql = <<-SQL
+        CREATE TABLE IF NOT EXISTS students (
+          id INTEGER PRIMARY KEY,
+          name TEXT,
+          grade INTEGER
+        );
+      SQL
+  end
 
 end
