696add7c1c1403e863c6b5cb55f1854960c03624
Adam Duff <j.adam.duff@gmail.com>
2018-08-29 00:14:09 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -56,8 +56,11 @@ class Student
   end
 
   def self.new_from_db(row)
-    student = Student.new(row[0], row[1], row[2])
-    student
+    Student.new(row[0], row[1], row[2])
   end
 
+  def find_by_name(name)
+    
+  end
+  
 end
